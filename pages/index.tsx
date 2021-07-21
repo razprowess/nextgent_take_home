89de195af9278f5cbabc9401efc1a6a7e8821ac0
=Abdulrazak Lawal <=razlaw5@gmail.com>
2021-07-21 22:45:27 +0100

    Implemented search feature to search for students

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,11 +33,20 @@ const Main: React.FC<Props> = ({ }) => {
     fetchStudents().then((apiStudents) => {
       setLoadingState('success')
       setStudents(apiStudents);
+      
     }).catch((error) => {
       setLoadingState('error');
     })
 
   }, []);
+
+  const filteredStudents = useMemo(()=>{ // filter students base on search query.
+
+    let query = searchQuery.toLowerCase();
+
+    return students.filter((student)=> student.first_name.toLowerCase().includes(query) || student.last_name.toLowerCase().includes(query));
+
+  },[searchQuery, students]);
 
 
   const onChangeHandler = (event: any) => {
@@ -51,7 +60,7 @@ const Main: React.FC<Props> = ({ }) => {
       <TextInput placeholder="type here" value={searchQuery} onChange={onChangeHandler} />
       
       {loadingState == 'success' && <Box direction="row" wrap={true}>
-        {students.map((s) => (
+        {filteredStudents.map((s) => (
           <Box margin="10px">
             <UserCard user={s} />
           </Box>
